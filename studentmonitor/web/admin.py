4f28811a7a838bd5a9fb631cdb46c7f76dab25c4
book <book2555@hotmail.co.th>
2019-02-16 22:34:49 +0700

    add

--- studentmonitor/web/admin.py
+++ studentmonitor/web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from  web.models import Student, Room, StudentInRoom, Teacher, Absent, Subject
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Room)
+admin.site.register(StudentInRoom)
+admin.site.register(Teacher)
+admin.site.register(Absent)
+admin.site.register(Subject)
--- studentmonitor/web/models.py
+++ studentmonitor/web/models.py
@@ -1,3 +1,53 @@
 from django.db import models
-
+from enum import Enum
 # Create your models here.
+class Student(models.Model):
+    class SexChoiceEnum(Enum):
+        male = '1'
+        fremale = '2'
+
+        @classmethod
+        def choices(cls):
+            return tuple((i.value, i.name) for i in cls)
+
+    first_name = models.CharField(max_length=200,blank=True, null=True)
+    last_name =  models.CharField(max_length=200,blank=True, null=True)
+    code = models.CharField(max_length=200,blank=True, null=True)
+    sex  = models.CharField(max_length=1, choices=SexChoiceEnum.choices(),blank=True, null=True)
+
+    def __str__(self):
+        return r"%s %s" %(self.first_name, self.last_name)
+class Room (models.Model):
+    name = models.CharField(max_length=200,blank=True, null=True)
+    description = models.CharField(max_length=200,blank=True, null=True)
+
+    def __str__(self):
+        return r"%s" %(self.name)
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=200,blank=True, null=True)
+    description = models.CharField(max_length=200,blank=True, null=True)
+    telephone = models.CharField(max_length=200,blank=True, null=True)
+
+    def __str__(self):
+        return r"%s" %(self.name)
+
+class Absent(models.Model):
+    name = models.CharField(max_length=200,blank=True, null=True)
+    description = models.CharField(max_length=200,blank=True, null=True)
+    def __str__(self):
+        return r"%s" %(self.name)
+
+class Subject(models.Model):
+    name = models.CharField(max_length=200,blank=True, null=True)
+    description = models.CharField(max_length=200,blank=True, null=True)
+
+    def __str__(self):
+        return r"%s" %(self.name)
+
+class StudentInRoom(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE,blank=True, null=True )
+    student = models.ForeignKey(Student, on_delete=models.CASCADE,blank=True, null=True )
+    current_year = models.IntegerField(blank=True, null=True,default=2561)
+    def __str__(self):
+        return r"%s (%s)" %(self.room, self.student)
